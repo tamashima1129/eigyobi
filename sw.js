c3f980c78c2bccabee469f8b998bb0b674b6a0f2
tamashima1129 <tamashima1129@gmail.com>
2026-03-11 01:50:19 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'eigyobi-v1';
+const CACHE = 'eigyobi-v2';
 const FILES = ['./index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
